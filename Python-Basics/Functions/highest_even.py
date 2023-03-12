2050205da2d5166d948a302940d2a8741d118054
William Barnes <jakebarnes1989@gmail.com>
2023-03-12 15:10:46 -0700

    highest even function exercise

---
+++ Python-Basics/Functions/highest_even.py
@@ -0,0 +1,12 @@
+def highest_even(li):
+    even_list = []
+
+    for item in li:
+        is_even = item % 2 == 0
+        if is_even:
+            even_list.append(item)
+
+    return max(even_list)
+
+
+print(highest_even([10, 7, 4, 6, 10, 11, 3, 12, 2, 8]))
