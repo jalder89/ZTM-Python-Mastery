73f7e1bb3626fbc329a3dca413c44e5bfdae4f97
William Barnes <jakebarnes1989@gmail.com>
2023-03-11 22:29:59 -0800

    Updated code to not use enumerate

--- Python-Basics/Loops/arrow_art.py
+++ Python-Basics/Loops/arrow_art.py
@@ -11,18 +11,21 @@ new_line = ""
 loop = True
 counter = 0
 
-for index, row in enumerate(picture):
+for row in picture:
     while loop == True:
-        for index, element in enumerate(row):
+        for element in row:
             if element == 0:
                 new_line += " "
             elif element == 1:
                 new_line += "*"
+
         new_line += " " * 8
         counter += 1
 
+        # Controls the number of times the image should be drawn before breaking
         if counter >= 4:
             loop = False
+
     print(new_line)
     new_line = ""
     loop = True
